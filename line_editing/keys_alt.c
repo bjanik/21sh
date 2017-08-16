4f7a481739dfa274be8308d560054e0a197d0fac
Bartlomiej JANIK <bjanik@localhost.localdomain>
2017-08-16 22:24:18 +0200

    OK

--- line_editing/keys_alt.c
+++ line_editing/keys_alt.c
@@ -2,5 +2,14 @@
 
 int	handle_alt_great(t_input *input)
 {
-	
+	input->history->current = input->history->head;
+	cp_history_to_buffer(input);
+	return (0);
+}
+
+int	handle_alt_less(t_input *input)
+{
+	input->history->current = input->history->end;
+	cp_history_to_buffer(input);
+	return (0);
 }
