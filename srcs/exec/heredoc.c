aea30e975cd5c1b4319a93a70e44071306c63662
Bartlomiej JANIK <bjanik@localhost.localdomain>
2017-11-07 22:54:30 +0100

    Need free heredoc lst

--- srcs/exec/heredoc.c
+++ srcs/exec/heredoc.c
@@ -12,26 +12,36 @@
 
 #include "bsh.h"
 
-static int		write_to_heredoc_pipe(t_redir *redir)
+static int		save_heredoc(t_redir *redir)
 {
-	t_bsh	*bsh;
+	t_input	*input;
+	t_list	*here_in;
 
-	bsh = get_bsh();
+	input = get_bsh()->input;
 	while (42)
 	{
-		display_prompt(bsh->input);
-		waiting_for_input(bsh->input);
-		bsh->input->buffer[--bsh->input->buffer_len] = '\0';
-		if (!ft_strcmp(bsh->input->buffer, redir->here_end))
+		display_prompt(input);
+		waiting_for_input(input);
+		input->buffer[--input->buffer_len] = '\0';
+		if (!ft_strcmp(input->buffer, redir->here_end))
 			break ;
-		bsh->input->buffer[bsh->input->buffer_len++] = '\n';
-		write(redir->heredoc_pipe[WRITE], bsh->input->buffer,
-				bsh->input->buffer_len);
+		input->buffer[input->buffer_len++] = '\n';
+		here_in = ft_lstnew(input->buffer, input->buffer_len);
+		if (redir->heredoc_input[0] == NULL)
+		{
+			redir->heredoc_input[0] = here_in;
+			redir->heredoc_input[1] = here_in;
+		}
+		else
+		{
+			redir->heredoc_input[1]->next = here_in;
+			redir->heredoc_input[1] = here_in;
+		}
 	}
 	return (0);
 }
 
-int		process_heredoc(t_exec *exec)
+int		save_heredoc_input(t_exec *exec)
 {
 	t_redir	*rd;
 
@@ -39,11 +49,7 @@ int		process_heredoc(t_exec *exec)
 	while (rd)
 	{
 		if (rd->here_end)
-		{
-			pipe(rd->heredoc_pipe);
-			write_to_heredoc_pipe(rd);
-			exec->is_heredoc = 1;
-		}
+			save_heredoc(rd);
 		rd = rd->next;
 	}
 	return (0);
@@ -51,7 +57,19 @@ int		process_heredoc(t_exec *exec)
 
 int		redir_heredoc(t_redir *redir)
 {
+	t_list	*here_data;
+
+	here_data = redir->heredoc_input[0];
+	if (pipe(redir->heredoc_pipe))
+		exit(EXIT_FAILURE);
+	while (here_data)
+	{
+		write(redir->heredoc_pipe[WRITE], here_data->content,
+			 here_data->content_size);
+		here_data = here_data->next;
+	}
 	if (dup2(redir->heredoc_pipe[READ], STDIN) < 0)
+		exit(EXIT_FAILURE);
 	close(redir->heredoc_pipe[WRITE]);
 	close(redir->heredoc_pipe[READ]);
 	return (0);
